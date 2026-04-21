812856fe10ac103f8ea1e1608ccd79a5d7aa8f8c
stevehong12 <stevehong124@gmail.com>
2026-04-21 16:06:29 +0900

    chore: gitignore 및 eslint 설정 (#2)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,20 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  prettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    rules: {
+      'no-unused-vars': 'warn', // 안 쓰는 변수 경고
+      'no-console': 'warn', // console.log 경고
+      'prefer-const': 'error', // let 대신 const 강제
+    },
+  },
+])
 
-export default eslintConfig;
+export default eslintConfig
